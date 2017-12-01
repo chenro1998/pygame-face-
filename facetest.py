78c30672059b1977a3c7daf14530b987b6137281
Kevin Chen <kevin@iMac7.local>
2017-12-01 11:46:49 -0500

    This one is final version

--- facetest.py
+++ facetest.py
@@ -1,10 +1,14 @@
-import face
+# Kevin Chen
+# 12/01/17
+# This fuction make the pygame shape about face in a pygame window
 
-import pygame,sys
+import face
+import pygame
+import sys
 from pygame.locals import *
 
 pygame.init()
-mainSurface = pygame.display.set_mode((700,700),0,32)
+mainSurface = pygame.display.set_mode((700, 700), 0, 32)
 myface = face.Face(mainSurface)
 
 
